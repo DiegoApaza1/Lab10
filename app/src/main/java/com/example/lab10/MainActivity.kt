c5e0b84a2b080cf781aacc43e7d901be0f77b905
Dnilson Achahuanco Huarilloclla <dnilson.ah.7@gmail.com>
2024-11-18 14:59:45 -0500

    Configuracion del mainactivity

--- app/src/main/java/com/example/lab10/MainActivity.kt
+++ app/src/main/java/com/example/lab10/MainActivity.kt
@@ -12,36 +12,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab10.ui.theme.Lab10Theme
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.example.lab10.view.SeriesApp
+import com.example.lab10.ui.theme.Lab10Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Lab10Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    SeriesApp() // Llama a tu aplicación principal configurada en SeriesApp.kt
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab10Theme {
-        Greeting("Android")
     }
 }
